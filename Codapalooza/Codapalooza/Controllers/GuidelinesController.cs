67d427561847c38d74223c895ce23ca3bd66e1d6
Eric De Carufel <decarufe@users.noreply.github.com>
2011-05-01 21:18:03 +0000

    Most of the content is there. Styling is missing

---
+++ Codapalooza/Codapalooza/Controllers/GuidelinesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Codapalooza.Controllers
+{
+    public class GuidelinesController : Controller
+    {
+        //
+        // GET: /Guidelines/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
