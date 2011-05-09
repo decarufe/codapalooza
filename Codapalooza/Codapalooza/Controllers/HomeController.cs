ae6c9851e1ce77ac86d79167f025f2fdb78e1d02
Eric De Carufel <decarufe@users.noreply.github.com>
2011-05-09 18:10:51 +0000

    Removed some unused usings + redid the indent style

--- Codapalooza/Codapalooza/Controllers/HomeController.cs
+++ Codapalooza/Codapalooza/Controllers/HomeController.cs
@@ -1,23 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Codapalooza.Controllers
 {
-  public class HomeController : Controller
-  {
-    public ActionResult Index()
-    {
-      ViewBag.Message = "Welcome to ASP.NET MVC!";
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Message = "Welcome to ASP.NET MVC!";
 
-      return View();
-    }
+			return View();
+		}
 
-    public ActionResult About()
-    {
-      return View();
-    }
-  }
+		public ActionResult About()
+		{
+			return View();
+		}
+	}
 }
--- Codapalooza/Codapalooza/Controllers/ScheduleController.cs
+++ Codapalooza/Codapalooza/Controllers/ScheduleController.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Codapalooza.Controllers
 {
-    public class ScheduleController : Controller
-    {
-        //
-        // GET: /Schedule/
+	public class ScheduleController : Controller
+	{
+		//
+		// GET: /Schedule/
 
-        public ActionResult Index()
-        {
-            return View();
-        }
+		public ActionResult Index()
+		{
+			return View();
+		}
 
-    }
+	}
 }
