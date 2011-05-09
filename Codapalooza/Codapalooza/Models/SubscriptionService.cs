a44b7335e5b154bfaed822be9489e6f5e5046bad
Eric De Carufel <decarufe@users.noreply.github.com>
2011-05-09 02:56:56 +0000

    added countdown of remaining registration places

---
+++ Codapalooza/Codapalooza/Models/SubscriptionService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Codapalooza.Models
+{
+	public class SubscriptionService
+	{
+		private int _maximumNumberOfInscriptions;
+		private CodapaloozaEntities _db;
+
+		public SubscriptionService()
+		{
+			int.TryParse(ConfigurationManager.AppSettings["maximumNumberOfInscriptions"], out _maximumNumberOfInscriptions);
+      _db = new CodapaloozaEntities();
+    }
+
+		public int RemainingPlaces()
+		{
+			var numberOfInscribedParticipants = _db.Participants.Count();
+			return _maximumNumberOfInscriptions - numberOfInscribedParticipants;
+		}
+
+		public static SubscriptionService Current
+		{
+			get { return new SubscriptionService();}
+		}
+	}
+}
