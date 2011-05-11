19be00cc3b7b88e522e23aceb25564363454b984
Eric De Carufel <decarufe@users.noreply.github.com>
2011-05-11 16:53:19 +0000

    Added the pay button in the side bar when the user still needs to pay

--- Codapalooza/Codapalooza/Controllers/HomeController.cs
+++ Codapalooza/Codapalooza/Controllers/HomeController.cs
@@ -1,9 +1,24 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Codapalooza.Models;
+using Codapalooza.Models.Security;
 
 namespace Codapalooza.Controllers
 {
 	public class HomeController : Controller
 	{
+		private CodapaloozaEntities _db;
+		public IMembershipService MembershipService { get; set; }
+
+		protected override void Initialize(RequestContext requestContext)
+		{
+			if (MembershipService == null) { MembershipService = new AccountMembershipService(); }
+			if (_db == null) _db = new CodapaloozaEntities();
+			base.Initialize(requestContext);
+		}
+
 		public ActionResult Index()
 		{
 			ViewBag.Message = "Welcome to ASP.NET MVC!";
@@ -14,6 +29,12 @@ namespace Codapalooza.Controllers
 		public ActionResult About()
 		{
 			return View();
+		}
+
+		public ActionResult MustPay(Guid? id)
+		{
+			var participant = _db.Participants.Single(p => p.UserName == User.Identity.Name);
+			return PartialView("_MustPayButton", participant);
 		}
 	}
 }
